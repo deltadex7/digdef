7611d4ac4f2732c93707c20366528e60d7ba6040
Delta Dextra <54788149+deltadex7@users.noreply.github.com>
2020-11-06 23:01:50 +0800

    Add tetromino piece mechanics

--- src/main.c
+++ src/main.c
@@ -21,6 +21,8 @@
 
 #include "raylib.h"
 #include <time.h>
+#include <stdlib.h>
+#include "piece.h"
 
 int main()
 {
@@ -28,8 +30,17 @@ int main()
   //--------------------------------------------------------------------------------------
   int screenWidth = 1280;
   int screenHeight = 720;
+  bool firstPiece = true;
+  PieceState currentpiece;
+
+  time_t time;
 
   InitWindow(screenWidth, screenHeight, "game");
+  srand(&time);
+
+  int blockSize = 32;
+  int centerWidth = screenWidth / 2;
+  int centerHeight = screenHeight / 2;
 
   SetTargetFPS(60);
   //--------------------------------------------------------------------------------------
@@ -37,11 +48,24 @@ int main()
   // Main game loop
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
+    if (firstPiece)
+    {
+      currentpiece = NewPiece();
+      firstPiece = false;
+    }
     // Update
     //----------------------------------------------------------------------------------
     // TODO: Update your variables here
     //----------------------------------------------------------------------------------
 
+    if (IsKeyPressed('R'))
+      currentpiece = NewPiece();
+    if (IsKeyPressed('F'))
+      PieceRotate(&currentpiece, RIGHT);
+    if (IsKeyPressed('D'))
+      PieceRotate(&currentpiece, LEFT);
+    if (IsKeyPressed('S'))
+      PieceRotate(&currentpiece, DOUBLE);
     // Draw
     //----------------------------------------------------------------------------------
     BeginDrawing();
@@ -49,7 +73,15 @@ int main()
     {
       ClearBackground(DARKGRAY);
 
-      DrawText("Congrats! You created your first window!", 190, 200, 20, WHITE);
+      for (int i = 0; i < 4; ++i)
+      {
+        DrawRectangle(
+            centerWidth + (currentpiece.piece[i] % 5 - 2) * blockSize,
+            centerHeight + (currentpiece.piece[i] / 5 - 2) * blockSize,
+            blockSize, blockSize, RaylibGroundColor(currentpiece.color[i]));
+      }
+
+      // DrawText("Congrats! You created your first window!", 190, 200, 20, WHITE);
     }
 
     EndDrawing();
